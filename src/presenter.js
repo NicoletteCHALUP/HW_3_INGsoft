8f5922ba9ac47038344c22be7abaee080f3278bd
Nicolette <nicolette.chalup@ucb.edu.bo>
2024-02-08 10:31:52 -0400

    Commit implementacion de la multiplicacion

--- src/presenter.js
+++ src/presenter.js
@@ -1,10 +1,13 @@
 import sumar from "./sumador";
-
+import multiplicar from "./multiplicardor";
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
+const first2 = document.querySelector("#primer-numero2");
+const second2 = document.querySelector("#segundo-numero2");
 const form = document.querySelector("#sumar-form");
+const form2 = document.querySelector("#multiplicar-form");
 const div = document.querySelector("#resultado-div");
-
+const div2 = document.querySelector("#resultado-div2");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
@@ -12,4 +15,13 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  
+});
+
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber2 = Number.parseInt(first2.value);
+  const secondNumber2 = Number.parseInt(second2.value);
+  div2.innerHTML = "<p>" + multiplicar(firstNumber2, secondNumber2) + "</p>";
 });
