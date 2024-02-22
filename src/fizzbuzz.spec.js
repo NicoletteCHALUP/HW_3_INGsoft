51f6c605dec867558bbd019ffaef593e2b923e92
Nicolette <nicolette.chalup@ucb.edu.bo>
2024-02-22 10:13:21 -0400

    1er COMMIT oficial Imprimir una secuencia para un numero si genero un numero sin regla genera el mismo numero

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -1,8 +1,8 @@
-//import generarFizzBuzz from "./FizzBuzz.js";
+import generarFizzBuzz from "./FizzBuzz.js";
 
 describe("FizzBuzz", () => {
   it("deberia devolver la secuencia de un numero como el 1", () => {
-    expect(FizzBuzz(1)).toEqual("1");
+    expect(generarFizzBuzz(1)).toEqual("1");
   });
 });
 
