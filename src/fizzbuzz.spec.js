0821fe526ca9676d8fcbca78a399e15c8fad0e19
Nicolette <nicolette.chalup@ucb.edu.bo>
2024-02-22 10:17:48 -0400

    Imprimir una secuencia para un numero si genero un numero sin regla genera el mismo numero en este caso para un numero diferente como el 2

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -4,5 +4,9 @@ describe("FizzBuzz", () => {
   it("deberia devolver la secuencia de un numero como el 1", () => {
     expect(generarFizzBuzz(1)).toEqual("1");
   });
+
+  it("deberia devolver la secuencia de un numero diferente como el 2", () => {
+    expect(generarFizzBuzz(2)).toEqual("2");
+  });
 });
 
